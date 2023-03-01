252da08829b6f5edcd90e6e89095b6e20e964fa8
Pavel Svec <pavel.svec@itcrawl.cz>
2023-03-01 16:59:10 +0100

    Refactor & comments

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Numerics;
+using System.Dynamic;
 
 namespace Methods
 {
@@ -18,50 +19,56 @@ namespace Methods
         static int RecursPartSum(int[] a, int i)
         {
             if (i == 0)
-                return a[i];
+                return a[0];
             else
                 return a[i] + RecursPartSum(a, i - 1);
+            // sum = a[i] + (a[0] + a[1] + a[2] + ... + a[i-1]) =
+            //       a[i] + a[i-1] + (a[0] + a[1] + a[2] + ... + a[i-2]) =
+            //       ...
+            //       a[i] + a[i-1] + a[i-2] + a[i-3] + ... + a[0]
         }
 
-        // iterační součet řaday Sum(1/(n*n))
-        static double IterRadaSum(int n)
+        // iterační součet prvních n členů řady Sum(1/(n*n))
+        static double IterRadaSum(int pocetClenu)
         {
             double soucet = 0.0;
 
-            for (int i = 1; i <= n; i++)
+            for (int i = 1; i <= pocetClenu; i++)
                 soucet += 1.0 / (i * i);
 
             return soucet;
         }
 
-        static double RekurzRadaSum(int n)
+        // rekurzivní součet prvních n členů řady Sum(1/(n*n))
+        static double RekurzRadaSum(int pocetClenu)
         {
-            if (n == 1)
-                return 1;
+            if (pocetClenu == 1)
+                return 1.0 / 1.0 * 1.0;
             else
-                return 1.0 / (n * n) + RekurzRadaSum(n - 1);
+                return 1.0 / (pocetClenu * pocetClenu) + RekurzRadaSum(pocetClenu - 1);
         }
 
-        // iterační součet řaday Sum(1/(n*n)) s omezením přesnosti
-        static decimal IterRadaSumLimit(int n = 1, decimal eps = 1e-5m)
+        // iterační součet řaday Sum(1/(n*n)) se zanedbáním členů < 1e-5
+        static decimal IterRadaSumLimit(decimal minimalniClen = 1e-5m)
         {
             decimal clen = 1.0m, soucet = 0.0m;
 
-            for (int i = n; clen > eps; i++)
+            for (int i = 1; clen > minimalniClen; i++)
             {
                 clen = 1.0m / (i * i);
-                if (clen > eps)
+                if (clen > minimalniClen)
                     soucet += clen;
             }
             return soucet;
         }
 
-        static decimal RekurzRadaSumLimit(int n = 1, decimal eps = 1e-5m)
+        // rekurzivní součet řaday Sum(1/(n*n)) se zanedbáním členů < 1e-5
+        static decimal RekurzRadaSumLimit(int n = 1, decimal minimalniClen = 1e-5m)
         {
             decimal clen = 1.0m / (n * n);
 
-            if (clen > eps)
-                return clen + RekurzRadaSumLimit(n + 1, eps);
+            if (clen > minimalniClen)
+                return clen + RekurzRadaSumLimit(n + 1, minimalniClen);
             else
                 return 0.0m;
 
@@ -91,8 +98,35 @@ namespace Methods
             }
         }
 
+        static IEnumerable<int> CyclicIndex(int length, int start)
+        {
+            int i = start;
+            while(true) 
+            {
+                if (i == length)
+                    i = 0;
+                yield return i++;  // vrať hodnotu, ale nekonči s metodou (= yield)
+                                   // při dalším volání metody zde pokračuj 
+                Console.WriteLine(i);
+            }
+        }
+
+        static dynamic InitMe(string jmeno, int vek, double vyska )
+        {
+            // dynamic je datový typ, který se definuje "za chodu"
+            dynamic man = new ExpandoObject();
+            man.Name = jmeno;
+            man.Age = vek;
+            man.Height = vyska;
+            man.WriteMe = $"Name: {man.Name}\tAge: {man.Age}\tHeight: {man.Height}";
+
+            return man;
+        }
+
         static void Main(string[] args)
         {
+            // 1. generické metody
+            Console.WriteLine("1");
             Console.WriteLine(GetMax(2, 3));
             Console.WriteLine(GetMax("abc", "def"));
             Console.WriteLine(GetMax(6.28, 3.2));
@@ -101,10 +135,37 @@ namespace Methods
 
             int[] ints = { 1, 2, 3, 4, 5, };
             double[] doubles = { 10, 20, 30, 40, 50, 60, };
-
-            Console.WriteLine(GetSum(ints, 0, ints.Length-1));
+            Console.WriteLine(GetSum(ints, 0, ints.Length - 1));
             Console.WriteLine(GetSum(doubles, 1, 1));
             Console.WriteLine(GetSum(doubles, 1, 3));
+
+            // 2. částečný součet pole
+            Console.WriteLine("\n2");
+            Console.WriteLine(IterPartSum(ints, 3));
+            Console.WriteLine(RecursPartSum(ints, 3));
+
+            // 3. součet řady
+            Console.WriteLine("\n3");
+            Console.WriteLine(RekurzRadaSum(pocetClenu:350));
+            Console.WriteLine(IterRadaSum(pocetClenu:350));
+            Console.WriteLine(RekurzRadaSumLimit(minimalniClen:1e-5m));
+            Console.WriteLine(IterRadaSumLimit(minimalniClen:1e-5m));
+
+            // 4. return yield - iterátor
+            Console.WriteLine("\n4");
+            string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("Stiskni 'q' pro ukončení cyklu:");
+            foreach(var x in CyclicIndex(s.Length, 3))
+            {
+                Console.Write(s[x]);
+                if(Console.ReadKey(true).KeyChar == 'q')
+                    break;
+            }
+
+            // 5. typ dynamic
+            Console.WriteLine("\n\n5");
+            dynamic man = InitMe("Jan", 20, 202.5);
+            Console.WriteLine(man.WriteMe);
         }
     }
 }
